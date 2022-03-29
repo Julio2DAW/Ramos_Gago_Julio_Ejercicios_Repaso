25daa705a34d54b62405f63fe508a2dffb4e8cf7
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2022-03-29 14:10:10 +0200

    ejercicio array asociativo meses

---
+++ meses.php
@@ -0,0 +1,36 @@
+<?php
+
+    echo '<h1>Dias de la Semana</h1>';
+    echo '<h4>Practicar el count()</h4>';
+
+    $semana = array("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo");
+
+    foreach ($semana as $nombre_dia => $num_dia) {
+        echo '<br> El dia de la semana es '.$nombre_dia.' '.$num_dia;
+    }
+
+
+    echo '<h1>Meses del año</h1>';
+    echo '<h4>Array asociativo</h4>';
+
+    $meses = array(
+
+        'enero' => '31',
+        'febrero' => '28',
+        'marzo' => '31',
+        'abril' => '30',
+        'mayo' => '31',
+        'junio' => '30',
+        'julio' => '31',
+        'agosto' => '31',
+        'septiembre' => '30',
+        'octubre' => '31',
+        'noviembre' => '30',
+        'diciembre' => '31'
+    );
+
+    foreach ($meses as $mes => $dias) {
+
+        echo '<br>El mes de '.$mes.' tiene '.$dias.' dias.';
+    }
+?>
