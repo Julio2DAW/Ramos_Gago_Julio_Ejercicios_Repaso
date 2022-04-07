3dbc265e0af17045c21cbf34ea7f98f6050d2cec
Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2022-04-07 20:22:44 +0200

    terminar ejercicio con return

---
+++ tar_3t_03/prueba3/con_return/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Ejercicio Opcional POO</title>
+    </head>
+    <body>
+        
+        <h1>Esto son los meses del año y la cantidad de días que tiene cada uno</h1>
+    </body>
+</html>
+<?php
+
+    //LLamo al archivo meses.php con todo lo que este tiene.
+    require('meses.php');
+
+    //Creo una nueva variable que utiliza la clase Meses()
+    $objetoMeses = new Meses();
+
+    //Recoge el array retornado $dias_meses de la funcion introducir_valores(),
+    //y ejecuta la función introducir_valores().
+    $objetoMeses->introducir_valores();
+
+    //LLamo al atributo $dias_meses para poder visualizar los meses y los días.
+    //$objetoMeses->dias_meses.
+    foreach ($objetoMeses->dias_meses as $mes => $dias) {
+
+        echo '<br>El mes de '.$mes.' tiene '.$dias.' dias.';
+    }
+?>
